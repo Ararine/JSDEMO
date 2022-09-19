d62d9628ffe2fc9ae45e5bb52174218f56a1071c
Ararine <younadme0112@gmail.com>
2022-09-19 12:33:47 +0900

    20220919 | Add JSDEMO

---
+++ a.js
@@ -0,0 +1,9 @@
+// 구버젼
+window.onload = function () {
+  //Javascript 방식
+  console.log("Hello");
+  let data = document.getElementById("wrap");
+  console.log(data.InnerText);
+};
+
+// $(document).ready(function(){}) -jQuery 방식
